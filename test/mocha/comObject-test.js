8ecb85ac769c800d03749619a0ca7505ce2be555
Brett McCaffray <Brett@bmp.local>
2014-09-07 18:51:56 -0700

    edit out unused comments

--- test/mocha/comObject-test.js
+++ test/mocha/comObject-test.js
@@ -16,14 +16,3 @@ describe('unit test counter object', function(){
 		expect(runComObject.getValue()).equal(1);
 	});
 });
-
-
-// example of unit test
-// describe('Array', function(){
-// 	describe('#indexOf()', function(){
-// 		it('should return -1 when the value is not present', function(){
-// 			//assert.equal(-1, [1, 2, 3].indexOf(5));
-// 			expect([1, 2, 3].indexOf(5)).to.equal(-1);
-// 		});
-// 	});
-// });
